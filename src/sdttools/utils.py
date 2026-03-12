f07b3229817451135a86c1dbe4ef031ed165ce15
Fenway Powers <fppowers@protonmail.com>
2026-03-12 19:43:45 -0400

    add type hints in utils.py

--- src/sdttools/utils.py
+++ src/sdttools/utils.py
@@ -1,8 +1,11 @@
 import struct
+from typing import Iterator
 
-def get_u32_le(buf, offset):
-    return struct.unpack("<I", buf[offset:offset+4])[0]
 
-def chunks(data, size):
+def get_u32_le(buf: bytes, offset: int) -> int:
+    return struct.unpack("<I", buf[offset:offset + 4])[0]
+
+
+def chunks(data: bytes, size: int) -> Iterator[bytes]:
     for i in range(0, len(data), size):
         yield data[i:i + size]
